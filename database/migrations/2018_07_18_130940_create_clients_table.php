d302602c9238baa42f6751d9c6d885d3ae02036f
Joe-Simpson <the.fluff.lives.on@gmail.com>
2018-07-18 14:38:15 +0100

    clients table migration created

---
+++ database/migrations/2018_07_18_130940_create_clients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('first_name');
+            $table->string('surname');
+            $table->string('title');
+            $table->string('postcode');
+            $table->string('address')->nullable();
+            $table->date('dob');
+            $table->string('phone_number');
+            $table->string('id_verification_type');
+            $table->string('customer_banned')->default('false');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
